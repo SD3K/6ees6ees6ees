c8d659119a97e55bdddb8b0e5e5ebf8412746b13
Skipper Harris <skipdawg3000@gmail.com>
2022-01-10 13:54:44 -0800

    passing tests for 6ees6ees6ees

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,19 @@
-class RetiredForagerBee {
-  // TODO..
-};
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super();
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.treasureChest = [];
+    this.color = 'grey';
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble() {
+    let treasure = 'treasure';
+    this.treasureChest.push(treasure);
+  }
+}
